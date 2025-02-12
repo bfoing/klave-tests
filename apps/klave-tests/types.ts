e25e730b99aaa0fd4b985831cb81444ef9df8340
bfoing <bertrand@secretarium.org>
2025-02-12 20:59:19 +0000

    Maximum simplification of types

--- apps/klave-tests/types.ts
+++ apps/klave-tests/types.ts
@@ -6,28 +6,12 @@ export class ErrorMessage {
     message!: string;
 }
 
-
 @json
 export class UserVerifiableAttribute {
     value: string = "";
-    challenge: string = "";
-    //attempts!: u64[];
-    verified: bool = false;
-    //verifiers: string[] = [];
-    //lastVerificationTime: u64 = 0;
-}
-
-@json
-export class PushNotificationUserConfiguration {
-    token!: string;
-    encryptionKey!: Uint8Array;
 }
 
 @json
 export class User {
-    userId!: string;
-    devicePublicKeyHash!: string;
-    seedTOTP!: string;
     email!: UserVerifiableAttribute;
-    pushNotifCfg!: string;
 }
