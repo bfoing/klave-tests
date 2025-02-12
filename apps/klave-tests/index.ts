b2652b0df33a496dc1f2f5fcfc7243255974170f
bfoing <bertrand@secretarium.org>
2025-02-12 22:11:44 +0000

    Further tests with types arrays

--- apps/klave-tests/index.ts
+++ apps/klave-tests/index.ts
@@ -6,6 +6,12 @@ class A {
 }
 
 @json
+class A2 {
+    value: string = "";
+    blob!: Uint8Array;
+}
+
+@json
 class B {
     instA: A = new A;
 }
@@ -15,19 +21,32 @@ class C {
     instA!: A;
 }
 
+@json
+class D {
+    instA2!: A2;
+}
+
 /**
  * @query
  */
 export function test(): void {
 
     let objB = new B();
-    objB.instA.value = "whatever";
-
+    objB.instA.value = "whatever B";
     Notifier.sendJson<B>(objB);
 
     let objC = new C();
     objC.instA = new A();
-    objC.instA.value = "whatever";
+    objC.instA.value = "whatever C";
+    Notifier.sendJson<C>(objC);
 
+    let objD = new D();
+    objD.instA2 = new A2();
+    objD.instA2.value = "whatever D";
+    let arr = new Uint8Array(3);
+    arr[0] = 1;
+    arr[1] = 12;
+    arr[2] = 123;
+    objD.instA2.blob = arr;
     Notifier.sendJson<C>(objC);
 }
