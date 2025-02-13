59c48cafdb461c37b96f2eeaec7ab594afb96149
bfoing <bertrand@secretarium.org>
2025-02-13 12:17:50 +0000

    Further tests

--- apps/klave-tests/index.ts
+++ apps/klave-tests/index.ts
@@ -18,12 +18,24 @@ class C {
 @json
 class D {
     value: string = "";
-    blob!: Uint8Array;
+    blob: Uint8Array = new Uint8Array(0);
 }
 
 @json
 class E {
     instD!: D;
+    blob: Uint8Array = new Uint8Array(0);;
+}
+
+@json
+class F {
+    value: string = "";
+    blob!: Uint8Array;
+}
+
+@json
+class G {
+    instF!: F;
     blob!: Uint8Array;
 }
 
@@ -50,7 +62,14 @@ export function test(): void {
     let objE = new E();
     objE.blob = arr;
     objE.instD = new D();
-    objE.instD.value = "whatever D";
+    objE.instD.value = "whatever E";
     objE.instD.blob = arr;
     Notifier.sendJson<E>(objE);
+
+    let objG = new G();
+    objG.blob = arr;
+    objG.instF = new F();
+    objG.instF.value = "whatever G";
+    objG.instF.blob = arr;
+    Notifier.sendJson<G>(objG);
 }
