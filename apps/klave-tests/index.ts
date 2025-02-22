0747849c694a9de37e846505f4929966077ce8f0
bfoing <bertrand@secretarium.org>
2025-02-22 15:36:09 +0000

    Signing key only

--- apps/klave-tests/index.ts
+++ apps/klave-tests/index.ts
@@ -7,20 +7,22 @@ import { Crypto, Notifier } from '@klave/sdk';
 export function subtleCryptoECDSA(): void
 {
     // Generate EC key
-    let eccKeyResult = Crypto.Subtle.generateKey({namedCurve: "P-256"} as Crypto.EcKeyGenParams, true, ["sign", "verify"]);
+    let eccKeyResult = Crypto.Subtle.generateKey({namedCurve: "P-256"} as Crypto.EcKeyGenParams, true, ["sign"]);
     if (!eccKeyResult.data)
-        return Notifier.sendString(`can't generate key: '${eccKeyResult.err!.message}'`);
+        return Notifier.sendString(`Can't generate key: '${eccKeyResult.err!.message}'`);
 
-    // Sign and Verify
+    // Sign
     let ecdsaParams = {hash: "SHA2-256"} as Crypto.EcdsaParams;
     let data = String.UTF8.encode("Hello World");
     let signEcc = Crypto.Subtle.sign(ecdsaParams, eccKeyResult.data!, data);
     if (!signEcc.data)
-        return Notifier.sendString(`can't sign: '${signEcc.err!.message}'`);
+        return Notifier.sendString(`Can't sign: '${signEcc.err!.message}'`);
 
+    // Verify
     let verifyEcc = Crypto.Subtle.verify(ecdsaParams, eccKeyResult.data!, data, signEcc.data!);
     if (!verifyEcc.data)
-        return Notifier.sendString(`can't verify: '${verifyEcc.err!.message}'`);
+        return Notifier.sendString(`Can't verify: '${verifyEcc.err!.message}'`);
 
+    // Return
     return Notifier.sendString(`isValid: '${verifyEcc.data!.isValid}'`);
 }
